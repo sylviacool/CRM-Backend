97bbb09068889656c6a0d9fa0baf4f4a02fdc769
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-05-02 01:38:12 +0200

    Add create, update, and delete endpoints for Customer with validation and custom exceptions

--- src/main/java/com/customer/CRM_Backend/controller/CustomerController.java
+++ src/main/java/com/customer/CRM_Backend/controller/CustomerController.java
@@ -2,37 +2,48 @@ package com.customer.CRM_Backend.controller;
 
 import com.customer.CRM_Backend.entity.Customer;
 import com.customer.CRM_Backend.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
-    @Autowired
-    private CustomerService service;
+    private final CustomerService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getCustomerById(id));
     }
 
 
     @GetMapping
     public ResponseEntity<List<Customer>> getAllCustomers() {
-        List<Customer> customers  = service.getAllCustomer();
+        List<Customer> customers  = service.getAllCustomers();
        return ResponseEntity.status(HttpStatus.OK).body(customers);
  }
 
 
     @PostMapping
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+    public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer) {
         Customer customer1 = service.createCustomer(customer);
         return ResponseEntity.status(HttpStatus.CREATED).body(customer1);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id, @RequestBody Customer customer) {
+        return ResponseEntity.ok(service.updateCustomer(id, customer));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") long id) {
+        service.deleteCustomer(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     }
 
--- src/main/java/com/customer/CRM_Backend/service/CustomerService.java
+++ src/main/java/com/customer/CRM_Backend/service/CustomerService.java
@@ -3,17 +3,16 @@ package com.customer.CRM_Backend.service;
 import com.customer.CRM_Backend.entity.Customer;
 import com.customer.CRM_Backend.exception.ResourceNotFoundException;
 import com.customer.CRM_Backend.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class CustomerService {
 
-    @Autowired
-    private CustomerRepository repo;
+    private final CustomerRepository repo;
 
     public Customer createCustomer(Customer customer) {
        if (repo.existsByEmail(customer.getEmail())){
@@ -24,14 +23,45 @@ public class CustomerService {
 
     }
 
-    public List<Customer> getAllCustomer() {
+    public List<Customer> getAllCustomers() {
+
         return repo.findAll();
     }
+
 
     public Customer getCustomerById(long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
     }
+
+    public Customer updateCustomer(long id, Customer customer) {
+        Customer customer1 = repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
+
+        //updating
+        customer1.setFirstName(customer.getFirstName());
+        customer1.setLastName(customer.getLastName());
+        customer1.setEmail(customer.getEmail());
+
+        //save updated customer
+        return repo.save(customer1);
+    }
+
+    public void deleteCustomer(long id) {
+        Customer customer = repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
+
+        repo.deleteById(id);
+    }
+
+    // public String deleteCustomer(long id) {
+    //        Optional<Customer> optional = repo.findById(id);
+    //        if (optional.isPresent()) {
+    //             repo.deleteById(id);
+    //           return "Customer deleted successfully for id: "+id;
+    //        }
+    //        throw new ResourceNotFoundException("Customer not found with id: " + id);
+    //    }
 
 
 }
